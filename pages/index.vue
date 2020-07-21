a52473bb273310ccb894157a5002d92b62a0d607
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2020-07-21 11:17:33 -0300

    inital commit

---
+++ pages/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <section class="hero is-dark is-fullheight">
+    <!-- <div class="hero-head">
+      <header class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item">
+              <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+              <span />
+              <span />
+              <span />
+            </span>
+          </div>
+          <div id="navbarMenuHeroC" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item is-active">Home</a>
+              <a class="navbar-item">Examples</a>
+              <a class="navbar-item">Documentation</a>
+              <span class="navbar-item">
+                <a class="button is-success is-inverted">
+                  <span class="icon">
+                    <i class="fab fa-github" />
+                  </span>
+                  <span>Download</span>
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>-->
+
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title is-size-1">
+          Miguelangel Cabrera
+        </h1>
+        <h2 class="subtitle is-size-3 is-family-secondary has-text-weight-light">
+          Frontend Engineer
+        </h2>
+      </div>
+    </div>
+
+    <div class="hero-foot">
+      <nav class="tabs is-boxed is-pulled-right">
+        <div class="container">
+          <ul>
+            <li>
+              <a
+                href="https://github.com/mcabreradev"
+                target="_blank"
+                aria-label="github"
+                rel="noreferrer"
+              >
+                <i class="fab fa-github circle-icon" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/mcabreradev/"
+                target="_blank"
+                aria-label="linkedin"
+                rel="noreferrer"
+              >
+                <i class="fab fa-linkedin circle-icon" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/mcabreradev"
+                target="_blank"
+                aria-label="twitter"
+                rel="noreferrer"
+              >
+                <i class="fab fa-twitter circle-icon" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://api.whatsapp.com/send?phone=541122510921&text=Hi%20Migue%20i%20want%20to%20contact%20you&source=&data="
+                target="_blank"
+                aria-label="whatsapp"
+                rel="noreferrer"
+              >
+                <i class="fab fa-whatsapp circle-icon" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com/miguelangelcabrera_/"
+                target="_blank"
+                aria-label="instagram"
+                rel="noreferrer"
+              >
+                <i class="fab fa-instagram circle-icon" />
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:mcabrera.dev@gmail.com"
+                target="_blank"
+                aria-label="email"
+                rel="noreferrer"
+              >
+                <i class="fas fa-envelope circle-icon" />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
